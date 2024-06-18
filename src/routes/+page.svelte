07fada06c276a6844017491356fbe4e8868e32a4
PillowGit <goldentaco727@gmail.com>
2024-06-18 19:36:01 -0400

    Setting things up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,2 @@
-<p class="title-text">This is some display text</p>
-<p class="xl-text">This is some display text</p>
-<p class="large-text">This is some display text</p>
-<p class="small-text">This is some display text</p>
-<p class="xs-text">This is some display text</p>
+
+<p class="xl-text">Home page!</p>
---
+++ src/routes/font-sizes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  const font_sizes: string[] = 
+    ["title-text", "xl-text", "large-text", "small-text", "xs-text"];
+</script>
+
+{#each Object.entries(font_sizes) as [i, size]}
+  <p class={size}>{parseInt(i)+1}. This is an example of "{size}" text</p>
+{/each}
